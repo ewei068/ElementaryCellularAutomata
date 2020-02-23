d06f782db4ad4036bd49c005b660acda97990368
Elvis Wei <ewei068@berkeley.edu>
2020-02-23 00:16:56 -0800

    First Commit

---
+++ CA.java
@@ -0,0 +1,62 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Arrays;
+import java.lang.Math;
+import java.awt.Rectangle;
+import javax.swing.*;
+import java.util.Hashtable;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+
+public class CA {
+    public int _rule;
+    public int[] _current;
+    public int y = 0;
+    public Hashtable<Integer, Integer> _ruleDict = new Hashtable<Integer, Integer>();
+    public Random rand = new Random();
+
+    public CA(int rule, int[] start) {
+        _rule = rule;
+        _current = start;
+        makeRule();
+    }
+
+    void display(Graphics g) {
+        int[] next = new int[_current.length];
+        for (int i = 0; i < _current.length; i += 1) {
+            int total = 0;
+
+            if (_current[i] == 1) {
+                g.fillRect(i, y, 1, 1);
+            }
+
+            if (i == 0) {
+                total = _current[_current.length - 1]*4 + _current[i]*2 + _current[i+1];
+            } else if (i == _current.length - 1) {
+                total = _current[i-1]*4 + _current[i]*2 + _current[0];
+            } else {
+                total = _current[i-1]*4 + _current[i]*2 + _current[i+1];
+            }
+
+            next[i] = _ruleDict.get(total);
+        }
+        y += 1;
+        _current = next;
+        // _rule = rand.nextInt(256);
+        // makeRule();
+    }
+
+    void makeRule() {
+        boolean[] bits = new boolean[8];
+        for (int i = 7; i >= 0; i--) {
+            bits[i] = (_rule & (1 << i)) != 0;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            _ruleDict.put(i, (bits[i]) ? 1 : 0);
+        }
+
+        System.out.println(_rule + " = " + Arrays.toString(bits));
+    }
+}
---
+++ Drawing.java
@@ -0,0 +1,41 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+
+
+public class Drawing extends Canvas {
+    public int rule;
+    /*public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My Drawing");
+        Canvas canvas = new Drawing();
+        canvas.setSize(400, 800);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+     */
+    public void paint(Graphics g) {
+        //Rectangle bb = new Rectangle(100, 100, 200, 200);
+        //mickey(g, bb);
+        int[] start = new int[399];
+        start[199] = 1;
+        CA _ca = new CA(rule, start);
+
+        int x = 0;
+        while (_ca.y < 1000) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            _ca.display(g);
+        }
+    }
+}
